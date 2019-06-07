00ba9ff7cca0dfe562e15a3a672cb1900a681fb0
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-07 02:29:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,8 +81,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |event, supplies|
       if supplies.include?("BBQ")
-        
-
+        event
+      
 end
 
 
